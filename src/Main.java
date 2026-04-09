65d1868b24c07f030e5a391e183d46bcb6e5e9f4
Gustavo Roldão <gustavodsroldao@gmail.com>
2026-04-09 19:15:25 -0300

    fix: esqueci de commitar

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,27 @@ import domain.Product;
 void main() {
 
     Product product1 = new Product();
+    product1.setPrice(new BigDecimal("1000"));
+
+    Product product2 = new Product();
+    product2.setPrice(new BigDecimal("1000"));
+
+    Product product3 = new Product();
+    product3.setPrice(new BigDecimal("1000"));
 
     System.out.println(" -------------");
     product1.setName("Celular");
     product1.setSku("SMS-GLX-BR-18L-SM");
-    product1.setPrice(new BigDecimal("1000"));
     product1.setPrice(new BigDecimal("999"));
     System.out.println(" -------------");
 
     System.out.println(product1);
     System.out.println(" -------------");
 
-    Product product2 = new Product();
 
     System.out.println(" -------------");
     product2.setName("Computador");
     product2.setSku("SMS-GLX-BR-18L-SM");
-    product2.setPrice(new BigDecimal("1000"));
     product2.setPrice(new BigDecimal("999"));
     System.out.println(" -------------");
 
@@ -27,15 +31,11 @@ void main() {
     System.out.println(" -------------");
 
 
-    Product product3 = new Product();
-
     System.out.println(" -------------");
     product3.setName("Tablet");
     product3.setSku("SMS-GLX-BR-18L-SM");
-    product3.setPrice(new BigDecimal("1000"));
     product3.setPrice(new BigDecimal("999"));
     System.out.println(" -------------");
-
     System.out.println(product3);
     System.out.println(" -------------");
 }
